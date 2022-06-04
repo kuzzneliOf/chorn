bde71c1288850322dd3f9fc1e194d86645f1a805
KovalOleg <oleh.koval@ukma.edu.ua>
2022-06-04 16:39:10 +0300

    Node 1.1

--- Node.py
+++ Node.py
@@ -1,12 +1,26 @@
 class Node:
     def __init__(self, data):
-        pass
-
-    def getChild(self, root):
-        pass
-
-    def __str__(self):
-        pass
+        self.data = data
+        self.left = None
+        self.right = None
 
     def insert(self, data):
-        pass
+        if data < self.data:
+            if self.left:
+                self.left.insert(data)
+            else:
+                self.left = Node(data)
+        else:
+            if self.right:
+                self.right.insert(data)
+            else:
+                self.right = Node(data)
+
+    def toPrint(self):
+        res = ''
+        if self.left:
+            res += self.left.toPrint()+' '
+        res += str(self.data)+' '
+        if self.right:
+            res += self.right.toPrint() + ' '
+        return res
--- main.py
+++ main.py
@@ -1,2 +1,9 @@
-print('6')
-print('2')
+from Node import Node
+
+node = Node(12)
+node.insert(8)
+node.insert(4)
+node.insert(15)
+node.insert(2)
+node.insert(13)
+print(node.toPrint())
