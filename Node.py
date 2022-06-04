fb0263b0060d57abad312d44f6030910551e3b18
KovalOleg <oleh.koval@ukma.edu.ua>
2022-06-04 16:22:59 +0300

    Node 1.0

---
+++ Node.py
@@ -0,0 +1,12 @@
+class Node:
+    def __init__(self, data):
+        pass
+
+    def getChild(self, root):
+        pass
+
+    def __str__(self):
+        pass
+
+    def insert(self, data):
+        pass
