38f651c2fc0efe8d857597600d77b4c81f77f949
KovalOleg <oleh.koval@ukma.edu.ua>
2022-07-25 19:16:42 +0300

    BFS (на задачі)

--- testbfc/bfs.py
+++ testbfc/bfs.py
@@ -7,24 +7,12 @@ class Graph:
         self.edges.append(newEdge)
 
     def addAngle(self, fromEdge, toEdge):
-        self.angles.append(Connection(fromEdge, toEdge))
+        self.angles.append(tuple(fromEdge, toEdge))
 
     def neighbours(self, edge):
         s = []
         for conn in self.angles:
-            if conn.getFromEdge() == edge:
-                if not conn.getToEdge() in s:
-                    s.append(conn.getToEdge())
+            if conn[0] == edge:
+                if not conn[1] in s:
+                    s.append(conn[1])
         return s
-
-
-class Connection:
-    def __init__(self, edge1, edge2):
-        self.edgeFrom = edge1
-        self.edgeTo = edge2
-
-    def getFromEdge(self):
-        return self.edgeFrom
-
-    def getToEdge(self):
-        return self.edgeTo
--- testbfc/main.py
+++ testbfc/main.py
@@ -1,23 +1,34 @@
-from bfs import Graph
+n, s, f = tuple(map(int, input().split()))
 
 graph = Graph()
-edges = [1, 2, 3, 4, 5]
+edges = list(range(1, n + 1))
+
 for i in edges:
     graph.addEdge(i)
-angles = [(1, 2), (2, 1), (3, 1), (1, 3), (4, 2), (2, 4), (5, 1), (1, 5), (3, 4), (4, 3)]
+angles = []
+
+matrix = []
+
+for i in range(n):
+    matrix.append(tuple(map(int, input().split())))
+for i in range(n):
+    for j in range(n):
+        if matrix[i][j] == 1:
+            angles.append(tuple([i+1,j+1]))
+
+# angles = [(1, 2), (2, 1), (3, 1), (1, 3), (4, 2), (2, 4), (5, 1), (1, 5), (3, 4), (4, 3)]
 for i in angles:
     graph.addAngle(i[0], i[1])
-
-start = 4
-aim = 5
+#
+start = s
+finish = f
 queue = []
 visited = []
 queue.append([start, []])
 results = []
 while len(queue) > 0:
     current = queue.pop(0)
-    if current[0] == aim:
-        current[1].append(aim)
+    if current[0] == finish:
         results.append(current[1])
     else:
         visited.append(current[0])
@@ -26,4 +37,6 @@ while len(queue) > 0:
                 a = current[1].copy()
                 a.append(current[0])
                 queue.append((i, a))
-print(results)
+print(min(list(map(len,results))))
+
+
